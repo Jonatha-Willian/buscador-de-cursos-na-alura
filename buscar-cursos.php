19142eb7e3bd08a903654130c07c5b9b2530dd2c
Jonatha Santos <jonathasantos.dev@hotmail.com>
2025-04-28 16:09:24 -0300

    Adicionando arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 require 'vendor/autoload.php';
 
@@ -7,8 +8,10 @@ use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
-$buscador = new Buscador($client, $crawler);
-$cursos = $buscador->buscar('/cursos-online-programacao/php');
-foreach($cursos as $curso){
-    echo $curso;
+
+$buscardor = new Buscador($client, $crawler);
+$cursos = $buscardor->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    exibeMensagem($curso);
 }
